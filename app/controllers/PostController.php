9a22be8d3e01112f955e6d9ff9e20e8fec7f1f56
Gru <121221770+evg-83@users.noreply.github.com>
2023-05-02 23:55:39 +1000

    20 helper

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -2,7 +2,9 @@
 
 namespace Controller;
 
+use Core\Pager;
 use Core\Session;
+use Model\Post;
 
 /** Прямой путь к файлу будет заблокирован */
 defined('ROOTPATH') or exit('Доступ запрещен!');
@@ -11,17 +13,33 @@ defined('ROOTPATH') or exit('Доступ запрещен!');
 class PostController
 {
     use MainController;
-    
+
     /** общий метод */
-    public function index()
+    public function index($id = null)
     {
         $ses = new Session;
-        
+
         /** перенаправление если не залогинился */
         if (!$ses->is_logged_in()) {
             redirect('login');
         }
-        
+
+        /** pagination vars; разбиение на страницы */
+        $limit = 3;
+        $data['pager'] = new Pager($limit);
+        $offset = $data['pager']->offset;
+
+        $post = new Post;
+
+        /** get post row */
+        $data['post'] = $post->where(['id' => $id]);
+
+        //надо получить id юзера этого смс, прокрутив смс
+        if ($data['post']) {
+            $data['post'] = $post->add_user_data($data['post']);
+            $data['post'] = $data['post'][0];
+        }
+
         $this->view('post', $data);
     }
 }
--- app/views/post.view.php
+++ app/views/post.view.php
@@ -2,24 +2,10 @@
 
 <div class="row p-2 col-md-8 shadow mx-auto border rounded">
 
-    <div class="col-md-2 text-center">
+    <div class="my-3">
 
-        <a href="<?= ROOT ?>/profile/<?= $row->id ?>">
-            <span>
-                <img class="profile-image rounded-circle m-4" src="<?= get_image($row->image) ?>" style="width: 100px; height: 100px; object-fit: cover;">
-            </span>
-
-            <h5><?= esc($row->username) ?></h5>
-        </a>
-
-    </div>
-
-    <div class="col-md-10 my-3">
-
-        <?php if (!empty($posts)) : ?>
-            <?php foreach ($posts as $post) : ?>
-                <?php $this->view('post-small', ['post' => $post]) ?>
-            <?php endforeach; ?>
+        <?php if (!empty($post)) : ?>
+                <?php $this->view('post-full', ['post' => $post]) ?>
         <?php endif; ?>
 
         <!-- кнопки пагинации -->
